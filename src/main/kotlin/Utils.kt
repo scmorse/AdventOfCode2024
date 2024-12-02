f16089a7f6eb100c10a71dd62d1ad46fa2b0e71e
Stephen Morse <stephencalebmorse@gmail.com>
2024-12-02 12:56:04 -0500

    Use nice zipWithNext helper

--- src/main/kotlin/Utils.kt
+++ src/main/kotlin/Utils.kt
@@ -2,7 +2,3 @@ fun <T> List<T>.second(): T {
   if (size < 2) throw NoSuchElementException("List does not have two elements.")
   return this[1]
 }
-
-fun <T> List<T>.consecutivePairs(): Sequence<Pair<T, T>> = sequence {
-  for (i in indices.drop(1)) yield(get(i - 1) to get(i))
-}

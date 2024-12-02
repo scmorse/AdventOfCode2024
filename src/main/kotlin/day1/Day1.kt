8212655d800217a036d0b04325951f90f96b6c8c
Stephen Morse <stephencalebmorse@gmail.com>
2024-12-02 09:55:30 -0500

    Use raw file input

--- src/main/kotlin/day1/Day1.kt
+++ src/main/kotlin/day1/Day1.kt
@@ -1,21 +1,28 @@
 package day1
 
-import partitionIndexed
+import second
+import java.io.File
 import kotlin.math.abs
 
 fun main() {
-  val (list1, list2) = input.partitionIndexed { i, _ -> i % 2 == 0 }
+  val (list1: List<Long>, list2: List<Long>) = readInput()
 
   // Part 1
-  val distance = list1.zip(list2).fold(0) { acc, (a, b) ->
-    acc + abs(a - b)
-  }
-  println("Part 1 distance: $distance")
+  val distance = list1.sorted().zip(list2.sorted())
+    .sumOf { abs(it.first - it.second) }
+  println("Part 1 distance: $distance") // 1388114
 
   // Part 2
   val counts = list2.groupingBy { it }.eachCount()
-  val similarity = list1.fold(0) { acc, it ->
-    acc + it * counts.getOrDefault(it, 0)
-  }
-  println("Part 2 similarity: $similarity")
+  val similarity = list1.sumOf { it * (counts[it] ?: 0) }
+  println("Part 2 similarity: $similarity") // 23529853
+}
+
+private fun readInput(): Pair<List<Long>, List<Long>> {
+  return File("src/main/kotlin/day1/input.txt").readLines()
+    .map { line ->
+      val parts = line.split("   ").apply { require(size == 2) }
+      parts.first().toLong() to parts.second().toLong()
+    }
+    .unzip()
 }

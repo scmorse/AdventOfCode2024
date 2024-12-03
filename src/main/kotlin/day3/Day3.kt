20eccca537bd1eb476d100ba65b642370c64e953
Stephen Morse <stephencalebmorse@gmail.com>
2024-12-03 11:53:19 -0500

    Simplify by not tracking line number

--- src/main/kotlin/day3/Day3.kt
+++ src/main/kotlin/day3/Day3.kt
@@ -8,29 +8,24 @@ private val disableMulRegex = Regex("don't\\(\\)")
 
 // https://adventofcode.com/2024/day/3
 fun main() {
-  val input: List<String> = readInput()
+  val input: String = readInput()
 
   // Part 1
-  val mulCommands: List<MulCommand> = input.flatMapIndexed { lineNumber, line ->
-    mulRegex.findAll(line).map { matchResult ->
+  val mulCommands: Sequence<MulCommand> = mulRegex.findAll(input)
+    .map { matchResult ->
       MulCommand(
         product = matchResult.groupValues[1].toLong() * matchResult.groupValues[2].toLong(),
-        line = lineNumber,
         range = matchResult.range,
       )
     }
-  }
   println("Part 1 sum of products: ${mulCommands.sumOf { it.product }}") // 167650499
 
   // Part 2
-  val enableMulCommands = input.flatMapIndexed { lineNumber, line ->
-    enableMulRegex.findAll(line).map { EnableMulCommand(line = lineNumber, range = it.range) }
-  }
-  val disableMulCommands = input.flatMapIndexed { lineNumber, line ->
-    disableMulRegex.findAll(line).map { DisableMulCommand(line = lineNumber, range = it.range) }
-  }
-  val commands: List<RecognizedCommand> = (mulCommands + enableMulCommands + disableMulCommands)
-    .sortedWith(compareBy<RecognizedCommand> { it.line }.thenBy { it.range.first })
+  val enableMulCommands = enableMulRegex.findAll(input).map { EnableMulCommand(range = it.range) }
+  val disableMulCommands = disableMulRegex.findAll(input).map { DisableMulCommand(range = it.range) }
+  val commands: Sequence<RecognizedCommand> = (mulCommands + enableMulCommands + disableMulCommands)
+    .sortedBy { it.range.first }
+
 
   var enabled = true
   val sumOfEnabledProducts = commands.sumOf { command ->
@@ -44,16 +39,15 @@ fun main() {
   println("Part 2 sum of enabled products: $sumOfEnabledProducts") // 95846796
 }
 
+
 sealed class RecognizedCommand {
-  abstract val line: Int
   abstract val range: IntRange
 }
 
-data class MulCommand(val product: Long, override val line: Int, override val range: IntRange) : RecognizedCommand()
-data class EnableMulCommand(override val line: Int, override val range: IntRange) : RecognizedCommand()
-data class DisableMulCommand(override val line: Int, override val range: IntRange) : RecognizedCommand()
+data class MulCommand(val product: Long, override val range: IntRange) : RecognizedCommand()
+data class EnableMulCommand(override val range: IntRange) : RecognizedCommand()
+data class DisableMulCommand(override val range: IntRange) : RecognizedCommand()
 
-private fun readInput(): List<String> {
-  return File("src/main/kotlin/day3/input.txt").readLines()
-    .filter { it.isNotBlank() }
+private fun readInput(): String {
+  return File("src/main/kotlin/day3/input.txt").readText()
 }

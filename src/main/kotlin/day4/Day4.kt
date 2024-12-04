04f6529d8ecd6aae1c59dea80f50940d5c5c5bbb
Stephen Morse <stephencalebmorse@gmail.com>
2024-12-04 13:24:42 -0500

    Use Grid abstraction

--- src/main/kotlin/day4/Day4.kt
+++ src/main/kotlin/day4/Day4.kt
@@ -5,19 +5,17 @@ import java.io.File
 // https://adventofcode.com/2024/day/4
 fun main() {
   val input: List<String> = readInput()
+  val grid = Grid(input)
 
   // Part 1
   var countOfXMAS = 0
-  for ((lineNumber, line) in input.withIndex()) {
-    for ((offset, char) in line.withIndex()) {
-      if (char != 'X') continue
-      val start = Coordinate(x = offset, y = lineNumber)
-      for (direction in Direction.entries) {
-        if (input[start + direction * 1] != 'M') continue
-        if (input[start + direction * 2] != 'A') continue
-        if (input[start + direction * 3] != 'S') continue
-        countOfXMAS++
-      }
+  grid.forEachChar { char, coordinate ->
+    if (char != 'X') return@forEachChar
+    for (direction in Direction.entries) {
+      if (grid[coordinate + direction * 1] != 'M') continue
+      if (grid[coordinate + direction * 2] != 'A') continue
+      if (grid[coordinate + direction * 3] != 'S') continue
+      countOfXMAS++
     }
   }
   println("Part 1 count of XMAS: $countOfXMAS")
@@ -25,17 +23,33 @@ fun main() {
 
   // Part 2
   var countOfXShapedMAS = 0
-  for ((lineNumber, line) in input.withIndex()) {
-    for ((offset, char) in line.withIndex()) {
-      if (char != 'A') continue
-      val middle = Coordinate(x = offset, y = lineNumber)
-      if (input.getAdjacentCharsOnAxis(middle, Direction.NE) != setOf('M', 'S')) continue
-      if (input.getAdjacentCharsOnAxis(middle, Direction.NW) != setOf('M', 'S')) continue
-      countOfXShapedMAS++
-    }
+  grid.forEachChar { char, coordinate ->
+    if (char != 'A') return@forEachChar
+    if (grid.getAdjacentCharsOnAxis(coordinate, Direction.NE) != setOf('M', 'S')) return@forEachChar
+    if (grid.getAdjacentCharsOnAxis(coordinate, Direction.NW) != setOf('M', 'S')) return@forEachChar
+    countOfXShapedMAS++
   }
   println("Part 2 count of MAS in X shape: $countOfXShapedMAS")
   check(countOfXShapedMAS == 1933)
+}
+
+class Grid(private val input: List<String>) {
+  operator fun get(coordinate: Coordinate): Char? {
+    val line = input.getOrNull(coordinate.y) ?: return null
+    return line.getOrNull(coordinate.x)
+  }
+
+  fun getAdjacentCharsOnAxis(middle: Coordinate, direction: Direction): Set<Char> =
+    setOfNotNull(this[middle + direction * 1], this[middle + direction * -1])
+
+
+  fun forEachChar(block: (Char, Coordinate) -> Unit) {
+    for ((lineNumber, line) in input.withIndex()) {
+      for ((offset, char) in line.withIndex()) {
+        block(char, Coordinate(x = offset, y = lineNumber))
+      }
+    }
+  }
 }
 
 data class Coordinate(val x: Int, val y: Int)
@@ -50,14 +64,6 @@ data class Vector(val direction: Direction, val magnitude: Int) {
   val x: Int get() = magnitude * direction.x
   val y: Int get() = magnitude * direction.y
 }
-
-private operator fun List<String>.get(coordinate: Coordinate): Char? {
-  val line = this.getOrNull(coordinate.y) ?: return null
-  return line.getOrNull(coordinate.x)
-}
-
-private fun List<String>.getAdjacentCharsOnAxis(middle: Coordinate, direction: Direction): Set<Char> =
-  setOfNotNull(this[middle + direction * 1], this[middle + direction * -1])
 
 private operator fun Direction.times(steps: Int) = Vector(direction = this, magnitude = steps)
 private operator fun Coordinate.plus(vector: Vector) = Coordinate(x = x + vector.x, y = y + vector.y)

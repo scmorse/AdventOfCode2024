f7193a8a7de605d271cfc5635abf6a741bc4f2ae
Stephen Morse <stephencalebmorse@gmail.com>
2024-12-05 06:05:49 -0500

    Day 5 solution

--- src/main/kotlin/day5/Day5.kt
+++ src/main/kotlin/day5/Day5.kt
@@ -3,15 +3,51 @@ package day5
 import java.io.File
 
 fun main() {
-  val input: List<String> = readInput()
+  val (rules: Set<Rule>, manuals: List<Manual>) = readInput()
 
-  // Step 1
+  // Part 1
+  val (validManuals, invalidManuals) = manuals.partition { manual -> !manual.breaksAnyRule(rules) }
+  val sumOfMidpointsOfValidManuals = validManuals.sumOf { manual -> manual[manual.size / 2].toLong() }
+  println("Part 1 sum of midpoints of valid manuals: $sumOfMidpointsOfValidManuals")
+  check(sumOfMidpointsOfValidManuals == 4790L)
 
-  // Step 2
+  // Part 2
+  val sumOfMidpointsOfInvalidManuals = invalidManuals.sumOf { manual ->
+    manual.asSequence()
+      .sortedWith { a, b ->
+        if (rules.contains(Rule(before = a, after = b))) -1
+        else if (rules.contains(Rule(before = b, after = a))) 1
+        else 0
+      }
+      .elementAt(manual.size / 2).toLong()
+  }
+  println("Part 2 sum of midpoints of invalid manuals: $sumOfMidpointsOfInvalidManuals")
+  check(sumOfMidpointsOfInvalidManuals == 6319L)
 }
 
-private fun readInput(): List<String> {
-  return File("src/main/kotlin/day5/input.txt").readLines()
-    .map { it.trim() }
-    .filter { it.isNotBlank() }
+data class Rule(val before: String, val after: String)
+typealias Manual = List<String>
+
+fun Manual.breaksAnyRule(rules: Set<Rule>): Boolean {
+  for ((i, manual1) in withIndex()) {
+    for (manual2 in slice((i + 1)..lastIndex)) {
+      if (rules.contains(Rule(before = manual2, after = manual1))) return true
+    }
+  }
+  return false
+}
+
+private fun readInput(): Pair<Set<Rule>, List<Manual>> {
+  val rules = mutableSetOf<Rule>()
+  val manuals = mutableListOf<Manual>()
+  File("src/main/kotlin/day5/input.txt").readLines()
+    .mapNotNull { it.trim().ifBlank { null } }
+    .forEach { line ->
+      if (line.contains("|")) {
+        rules.add(line.split("|").let { (before, after) -> Rule(before, after) })
+      } else {
+        manuals.add(line.split(",").also { check(it.size % 2 == 1) })
+      }
+    }
+  return Pair(rules, manuals)
 }
--- src/main/kotlin/day6/Day6.kt
+++ src/main/kotlin/day6/Day6.kt
@@ -5,13 +5,12 @@ import java.io.File
 fun main() {
   val input: List<String> = readInput()
 
-  // Step 1
+  // Part 1
 
-  // Step 2
+  // Part 2
 }
 
 private fun readInput(): List<String> {
   return File("src/main/kotlin/day6/input.txt").readLines()
-    .map { it.trim() }
-    .filter { it.isNotBlank() }
+    .mapNotNull { it.trim().ifBlank { null } }
 }

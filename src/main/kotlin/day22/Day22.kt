10510e3498ff020404cd58cbe55208f4d68b9d00
Stephen Morse <stephencalebmorse@gmail.com>
2024-12-21 04:52:42 -0500

    Prep day 22

---
+++ src/main/kotlin/day22/Day22.kt
@@ -0,0 +1,15 @@
+package day22
+
+import java.io.File
+
+// https://adventofcode.com/2024/day/22
+fun main() {
+  val input: List<String> = readInput()
+  // Part 1
+  // Part 2
+}
+
+private fun readInput(): List<String> {
+  return File("src/main/kotlin/day22/input.txt").readLines()
+    .mapNotNull { it.trim().ifBlank { null } }
+}

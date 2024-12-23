7afee65175bace4a481e31a84579760a76dc20b0
Stephen Morse <stephencalebmorse@gmail.com>
2024-12-22 22:58:07 -0500

    Over-optimize for no reason whatsoever

--- src/main/kotlin/day22/Day22.kt
+++ src/main/kotlin/day22/Day22.kt
@@ -1,6 +1,7 @@
 package day22
 
 import java.io.File
+import kotlin.time.measureTime
 
 // https://adventofcode.com/2024/day/22
 fun main() {
@@ -14,38 +15,51 @@ fun main() {
   check(part1Answer == 17960270302L)
 
   // Part 2
-  val sumsByDiffs = mutableMapOf<List<Int>, SumAndIds>()
-  input.forEachIndexed { id, startingSecret ->
-    getPseudoRandomSequence(startingSecret)
-      .map { (it % 10).toInt() }
-      .windowed(5)
-      .forEach { groupOf5 ->
-        val diffs = groupOf5.zipWithNext().map { (a, b) -> a - b }
-        sumsByDiffs.getOrPut(diffs) { SumAndIds() }.addIfNew(id, groupOf5.last())
+  measureTime {
+    val sumsByDiffs = Array(130_321) { SumAndIds() }
+    input.forEachIndexed { id, startingSecret ->
+      val seq = getPseudoRandomSequence(startingSecret).map { it.toInt() % 10 }
+      var a: Int
+      var b = (seq[1] - seq[0] + 9) * 361
+      var c = (seq[2] - seq[1] + 9) * 19
+      var d = (seq[3] - seq[2] + 9)
+      (4..seq.lastIndex).forEach { i ->
+        a = b * 19
+        b = c * 19
+        c = d * 19
+        d = seq[i] - seq[i - 1] + 9
+        sumsByDiffs[a + b + c + d].addIfNew(id, seq[i])
       }
+    }
+    val mostBananas = sumsByDiffs.maxOf { it.sum }
+    println("Part 2 most bananas for any one instruction: $mostBananas")
+  }.also {
+    println(it)
   }
-  val mostBananas = sumsByDiffs.values.maxOf { it.sum }
-  println("Part 2 most bananas for any one instruction: $mostBananas")
-  check(mostBananas == 2042)
 }
 
-fun getPseudoRandomSequence(startingSecret: String): Sequence<Long> = sequence {
+fun getPseudoRandomSequence(startingSecret: String): List<Long> = buildList {
   var s = startingSecret.toLong()
-  yield(s)
+  add(s)
   repeat(2000) {
-    s = ((s * 64L) xor s) % 16777216L
-    s = ((s / 32L) xor s) % 16777216L
-    s = ((s * 2048L) xor s) % 16777216L
-    yield(s)
+    s = ((s shl 6) xor s) and 16777215L
+    s = ((s shr 5) xor s) and 16777215L
+    s = ((s shl 11) xor s) and 16777215L
+    add(s)
   }
 }
 
-class SumAndIds(
-  private val ids: MutableSet<Int> = mutableSetOf(),
-  var sum: Int = 0,
-) {
+class SumAndIds {
+  private val ids: Array<Long> = Array(34) { 0L }
+  var sum: Int = 0
   fun addIfNew(id: Int, value: Int) {
-    if (ids.add(id)) sum += value
+    val idx = id shr 6
+    val l = ids[idx]
+    val bit = 1L shl (id and 63)
+    if ((l and bit) == 0L) {
+      ids[idx] = (l or bit)
+      sum += value
+    }
   }
 }
 
